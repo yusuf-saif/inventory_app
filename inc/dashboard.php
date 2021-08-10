625425e8f3be70eda0ef48fc22d24dbe39215516
yusuf-saif <ysaifullah28kbg.com>
2021-08-10 15:50:10 +0100

    Added new files

---
+++ inc/dashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+      <title>Dashboard</title>
+</head>
+<body>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Category</a>
+  <a href="#">Products</a>
+  <a href="#">Suppliers</a>
+  <a href="#">Users</a>
+  <a href="#">Brand</a>
+  <a href="#">Purchase</a>
+  <a href="#">Customer</a>
+</div>
+
+<!-- Use any element to open the sidenav -->
+<span onclick="openNav()">open</span>
+</body>
+</html>
